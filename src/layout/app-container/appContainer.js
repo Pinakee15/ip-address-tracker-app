94a7b9db1e09d341b3d76ad424a232b4225b10f3
PinakeeKaushik <pinakee.kaushik@likeminds.community>
2021-10-10 16:32:08 +0530

    built data passing between app container and display modal

--- src/layout/app-container/appContainer.js
+++ src/layout/app-container/appContainer.js
@@ -11,11 +11,14 @@ const style = {
 const AppContainer = (props) => {
 
     const [inputIp , setInputIp] = useState('');
+    const [ipInfos , setIpInfos] = useState({});
+    // const [ipInfos , setIpInfos] = useState("string");
     
     useEffect(()=>{
-        // fetchIpLocations(inputIp).then(res=>{
-        //     console.log("THIS IS THE AXIOS RESPONSE : ", res?.data);
-        // })
+        fetchIpLocations(inputIp).then(res=>{
+            setIpInfos({...res?.data})
+            // setIpInfos(`${Math.random()}`);
+        })
     }, [inputIp])
 
 	return (
@@ -25,11 +28,13 @@ const AppContainer = (props) => {
             <div className="app-container__upper-body" style={style}>
                 <h1 className="upper-body__heading">IP Address Tracker</h1>
                 <IpInputSearch setInputIp={setInputIp}/>
-                <DisplayDetailsModal />
+                <DisplayDetailsModal ipInfos={ipInfos} />
             </div>
 
             <div className="app-container__lower-body">
-
+                <div>
+                    lower body
+                </div>          
             </div>         
             {/* <div>Ip input : {inputIp}</div> */}
         </div>
--- src/shared/entryComponents/Modals/displayDetailsModal/displayDetailsModal.js
+++ src/shared/entryComponents/Modals/displayDetailsModal/displayDetailsModal.js
@@ -1,55 +1,55 @@
 import React, { useState } from 'react';
 import './displayDetailsModal.css';
 
-let ipInfos = {
-    "ip": "31.13.70.36",
-    "location": {
-        "country": "US",
-        "region": "California",
-        "city": "Los Angeles",
-        "lat": 34.05223,
-        "lng": -118.24368,
-        "postalCode": "90001",
-        "timezone": "-07:00",
-        "geonameId": 5368361
-    },
-    "domains": [
-        "edge-star-mini-shv-01-lax3.facebook.com",
-        "facebook.com",
-        "fb.com",
-        "fb.me",
-        "fbcdn.net"
-    ],
-    "as": {
-        "asn": 32934,
-        "name": "Facebook Inc",
-        "route": "31.13.70.0/24",
-        "domain": "https://www.facebook.com/",
-        "type": "Content"
-    },
-    "isp": "Facebook Ireland Ltd",
-    "proxy": {
-        "proxy": false,
-        "vpn": false,
-        "tor": false
-    }
+// let ipInfos = {
+//     "ip": "31.13.70.36",
+//     "location": {
+//         "country": "US",
+//         "region": "California",
+//         "city": "Los Angeles",
+//         "lat": 34.05223,
+//         "lng": -118.24368,
+//         "postalCode": "90001",
+//         "timezone": "-07:00",
+//         "geonameId": 5368361
+//     },
+//     "domains": [
+//         "edge-star-mini-shv-01-lax3.facebook.com",
+//         "facebook.com",
+//         "fb.com",
+//         "fb.me",
+//         "fbcdn.net"
+//     ],
+//     "as": {
+//         "asn": 32934,
+//         "name": "Facebook Inc",
+//         "route": "31.13.70.0/24",
+//         "domain": "https://www.facebook.com/",
+//         "type": "Content"
+//     },
+//     "isp": "Facebook Ireland Ltd",
+//     "proxy": {
+//         "proxy": false,
+//         "vpn": false,
+//         "tor": false
+//     }
+// }
+
+let formatIpInfos = (ipInfos)=>{
+    return [
+            {'IP ADDRESS' : ipInfos['ip']} , 
+            {'LOCATION' : (ipInfos?.location?.city ? ipInfos?.location?.city + ', ' : '') + 
+                (ipInfos?.location?.region ? ipInfos?.location?.region + ', ' : '') + 
+                (ipInfos?.location?.country ? ipInfos?.location?.country : '')},
+            {'TIMEZONE' : ipInfos?.location?.timezone} , {'ISP' : ipInfos['isp']}
+        ]
 }
-
-ipInfos = [{'IP ADDRESS' : ipInfos['ip']} , 
-
-                {'LOCATION' : (ipInfos['location']['city'] ? ipInfos['location']['city'] + ', ' : '') + 
-                    (ipInfos['location']['region'] ? ipInfos['location']['region'] + ', ' : '') + 
-                    (ipInfos['location']['country'] ? ipInfos['location']['country'] : '')},
-
-                 {'TIMEZONE' : ipInfos['location']['timezone']} , {'ISP' : ipInfos['isp']}]
 
 const DisplayDetailsModal = (props) => {
 
-    //const [ipInfo, setIpInfo] = useState('');
-    
 	return (
         <div className="modalContainer">
-            {ipInfos.map(info=>{
+            {formatIpInfos(props?.ipInfos).map(info=>{
                 return (
                     <div>
                         <p>{Object.keys(info)[0]}</p>
