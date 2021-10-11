94ce5279f06fbb562667a959dead98e063ec4d44
PinakeeKaushik <pinakee.kaushik@likeminds.community>
2021-10-11 13:51:51 +0530

    login styling done

--- src/layout/login-component/loginComponent.js
+++ src/layout/login-component/loginComponent.js
@@ -1,18 +1,14 @@
-import React, { useEffect } from 'react'
+import React, { useEffect } from 'react';
 import Sawo from 'sawo'
+import './loginComponent.css'
 
 const LoginComponent = () => {
     useEffect(() => {
         var config = {
-            // should be same as the id of the container created on 3rd step
             containerID: 'sawo-container',
-            // can be one of 'email' or 'phone_number_sms'
             identifierType: 'phone_number_sms',
-            // Add the API key copied from 5th step
-            apiKey: '06e99332-df38-4456-abf7-59fe16f3229a',
-            // Add a callback here to handle the payload sent by sdk
-            onSuccess: payload => {
-                // you can use this payload for your purpose
+            apiKey: '06e99332-df38-4456-abf7-59fe16f3229a',        
+            onSuccess: payload => {                
                 console.log("THIS IS THE SAWO PAYLOAD : ", payload);
             },
         }
@@ -22,7 +18,13 @@ const LoginComponent = () => {
 
     return (
         <div className="container">
-            <div id="sawo-container" style={{ height: '300px', width: '315px', borderRadius: '50px' }}></div>
+            <div className="container__login-box">
+                <div id="sawo-container" 
+                    style={{ height: '340px',  width: '320px',
+                    borderRadius: '50px'}}>
+                </div>
+            </div>
+            <h1 className="container-header">Please login to access the app !</h1>
         </div>
     )
 }
